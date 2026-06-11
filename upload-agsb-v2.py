0eba9f29b4916b89884e7abcebd7cb193ab26596
lijiaxing16 <97070645+lijiaxing16@users.noreply.github.com>
2026-06-11 14:29:23 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -33,7 +33,7 @@ CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的�
 # ====== 全局可配置参数（可直接在此处修改） ======
 USER_NAME = "ljc666"         # 用户名
 UUID = "e13ccb69-3db6-4fe0-a7ef-a980f4c60b12"                     # UUID，留空则自动生成
-PORT = "8006"                    # Vmess端口，留空或0则自动生成
+PORT = 57879                    # Vmess端口，留空或0则自动生成
 DOMAIN = ""                   # 域名，留空则自动获取
 CF_TOKEN = ""                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
